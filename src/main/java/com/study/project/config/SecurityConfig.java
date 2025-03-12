81284b7fa40da08b2d5f664a7f53b63ea322f921
woochul <sksksk96@naver.com>
2025-03-12 17:54:21 +0900

    sec안돼는 문제는 security문제였음 아근데 진도가 너무안나간다...

--- src/main/java/com/study/project/config/SecurityConfig.java
+++ src/main/java/com/study/project/config/SecurityConfig.java
@@ -5,22 +5,48 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer {
+
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/","/node_modules/**","/sign-up", "/check-email", "/check-email-token", "/email-login", "/check-email-login", "/login-link", "/static/**").permitAll()
-                        .anyRequest().authenticated());
+                        .requestMatchers("/", "/node_modules/**", "/sign-up", "/check-email", "/check-email-token", "/email-login", "/check-email-login", "/login-link", "/static/**", "/images/**").permitAll()
+                        .anyRequest().authenticated())
+                .headers(headers -> headers
+                        .contentTypeOptions(HeadersConfigurer.ContentTypeOptionsConfig::disable) // MIME 타입 필터링 해제
+                )
+                .addFilterBefore(new SecurityContextPersistenceFilter(), BasicAuthenticationFilter.class)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/node_modules/**")
+                .addResourceLocations("file:src/main/resources/static/node_modules/")
+                .setCachePeriod(3600)
+                .resourceChain(true);
+
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:src/main/resources/static/images/")
+                .setCachePeriod(3600)
+                .resourceChain(true);
+    }
 
 }
